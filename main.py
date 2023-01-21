65b57d77f97c9c2f67f46eb2bb90102f81315683
NirVar <nvarsanno@gmail.com>
2023-01-21 14:49:44 +0200

    Goodluck

--- main.py
+++ main.py
@@ -3,9 +3,7 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 import matplotlib.pyplot as plt
-from keras.utils import load_img
 import tensorflow_datasets as tfds
-from keras.preprocessing import image
 
 cars196 = tfds.load('Cars196', as_supervised=True, shuffle_files=True)
 cars_train = cars196['train']
@@ -16,4 +14,4 @@ df = pd.DataFrame(values, columns=['value'])
 df.to_csv('test')
 
 df = pd.read_csv('test')
-nir = 'homo'
+Oron = 'homo'
